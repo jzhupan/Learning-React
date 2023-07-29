1fcdd50579d38719de90c0346064c9698300abf6
jzhupan <jzhupan@gmail.com>
2023-07-29 15:39:41 -0400

    Accomplished on click button with an on close alert.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisibility, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>{" "}
-      </Alert>
-      <Button />
+      {alertVisibility && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        Click Me
+      </Button>
     </div>
   );
 }
--- react-app/src/components/Button.tsx
+++ react-app/src/components/Button.tsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <button className="btn btn-primary">Click Me</button>;
+interface Props {
+  children: string;
+  color?: "primary" | "secondary" | "danger" | "success";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
