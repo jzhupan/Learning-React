55b3cfe710c86d4436099524173eeae768e8f842
jzhupan <jzhupan@gmail.com>
2023-08-01 11:35:26 -0400

    Player around with css, and react-icons, made a like button.

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,32 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
+import { BsFillCalendarDayFill } from "react-icons/bs";
+import Like from "./Like";
 
 function App() {
-  const [alertVisibility, setAlertVisibility] = useState(false);
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const [alertVisibility, setAlertVisibility] = useState(false);
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      {alertVisibility && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      {/* {alertVisibility && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          This is an alert, you have clicked the button
+        </Alert>
       )}
-
       <Button color="primary" onClick={() => setAlertVisibility(true)}>
-        Click Me
-      </Button>
+        My Button
+      </Button> */}
+      <Like onClick={() => console.log("Clicked")} />
+      {/* <BsFillCalendarDayFill color="red" size="40" />
+      <ListGroup items={items} heading="Cities" onSelectItem={() => {}} /> */}
     </div>
   );
 }
